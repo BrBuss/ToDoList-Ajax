8ae3c13e0cad614b35256f78cde7739e8079b87b
Bruno Buss <brunooliveira9895@gmail.com>
2020-08-20 08:34:10 +0100

    Frontend App

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'todoapi',
+    'frontend',
 
     'rest_framework',
 ]
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
